aebf8a3b45382501e937dd0ec7e21b85877e78d0
Bernardo <bpmsilva@gmail.com>
2025-06-22 02:45:52 -0300

    adiciona o Binding App

---
+++ SolucaoBindingApp/app/src/main/java/com/example/bindingapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.bindingapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.bindingapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        // setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+
+        // listener
+        binding.button.setOnClickListener(v -> {
+            binding.textView.setText("Button clicked!");
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
