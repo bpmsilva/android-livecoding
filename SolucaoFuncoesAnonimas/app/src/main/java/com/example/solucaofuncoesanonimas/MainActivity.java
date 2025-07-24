363930e6477f23e94fb0ab6c4892599991b6a859
Bernardo <bpmsilva@gmail.com>
2025-07-24 14:58:16 -0300

    atualiza o código de funções anônimas para incluir outras alternativas

--- SolucaoFuncoesAnonimas/app/src/main/java/com/example/solucaofuncoesanonimas/MainActivity.java
+++ SolucaoFuncoesAnonimas/app/src/main/java/com/example/solucaofuncoesanonimas/MainActivity.java
@@ -11,7 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +19,34 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        Button clickButton = findViewById(R.id.click_button);
-        clickButton.setOnClickListener(v -> {
-            Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show();
+        // Using a lambda expression
+        Button anonymousClassButton = findViewById(R.id.anonymous_class_button);
+        Button lambdaFunctionButton = findViewById(R.id.lambda_class_button);
+        Button customClassButton = findViewById(R.id.custom_class_button);
+        Button activityClassButton = findViewById(R.id.activity_class_button);
+
+        // Using an anonymous class
+        anonymousClassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(
+                        MainActivity.this,
+                        "Anonymous class clicked!",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+
+
+        lambdaFunctionButton.setOnClickListener(v -> {
+            Toast.makeText(this, "Anonymous function clicked!", Toast.LENGTH_SHORT).show();
         });
 
         // Using a custom listener class
-        // clickButton.setOnClickListener(new MyCustomListener());
+        customClassButton.setOnClickListener(new MyCustomListener());
+
+        // Using the activity class as a listener
+        activityClassButton.setOnClickListener(this);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -34,10 +55,17 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.activity_class_button) {
+            Toast.makeText(MainActivity.this, "Activity button clicked!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private class MyCustomListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            Toast.makeText(MainActivity.this, "Custom listener clicked!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Custom class button clicked!", Toast.LENGTH_SHORT).show();
         }
     }
 }
