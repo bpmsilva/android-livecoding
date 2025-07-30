8d063420673802aa39ab2ab2f939c2efe6360dc7
Bernardo <bpmsilva@gmail.com>
2025-07-30 12:31:08 -0300

    adiciona a solcao de ProgressBar

---
+++ SolucaoProgressBar/app/src/main/java/com/example/solucaoprogressbar/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.solucaoprogressbar;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressIndeterminate, progressDeterminate;
+    private Button startButton;
+    private Handler handler = new Handler();
+    private int progresso = 0;
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            progresso += 10;
+            progressDeterminate.setProgress(progresso);
+
+            if (progresso < 100) {
+                handler.postDelayed(this, 1000); // chama novamente após 300ms
+            } else {
+                progressIndeterminate.setVisibility(ProgressBar.GONE);
+                Toast.makeText(MainActivity.this, "Concluído!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressIndeterminate = findViewById(R.id.progress_indeterminate);
+        progressDeterminate = findViewById(R.id.progress_determinate);
+        startButton = findViewById(R.id.start_button);
+
+        startButton.setOnClickListener(v -> iniciarCarregamento());
+    }
+
+    private void iniciarCarregamento() {
+        progresso = 0;
+        progressDeterminate.setProgress(0);
+        progressIndeterminate.setVisibility(ProgressBar.VISIBLE);
+
+        handler.postDelayed(runnable, 300);
+    }
+}
