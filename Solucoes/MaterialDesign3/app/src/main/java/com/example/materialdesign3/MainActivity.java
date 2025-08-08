d64726951c84adf905af480c983ed46e25dfc2ff
Bernardo <bpmsilva@gmail.com>
2025-08-08 20:10:28 -0300

    adicona projeto de Material Design

---
+++ Solucoes/MaterialDesign3/app/src/main/java/com/example/materialdesign3/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.materialdesign3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button buttonNext = findViewById(R.id.button_next);
+        buttonNext.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, NoMaterialActivity.class);
+            startActivity(intent);
+        });
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+}
