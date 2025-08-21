82942ffa142f313fcc9e8b3cb0f516ee23344b26
Bernardo <bpmsilva@gmail.com>
2025-08-21 14:52:42 -0300

    adiciona projeto que mostra mudança de orientação de tela

---
+++ Solucoes/Orientacao/app/src/main/java/com/example/orientacao/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.orientacao;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(this, "Landscape mode", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Portrait mode", Toast.LENGTH_LONG).show();
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
