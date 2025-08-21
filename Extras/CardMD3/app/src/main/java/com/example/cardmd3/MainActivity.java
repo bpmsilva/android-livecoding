9bbd6811971425669fe5bbbbacb5e986fd366915
Bernardo <bpmsilva@gmail.com>
2025-08-21 16:51:45 -0300

    adiciona popup menu no exemplo do Card

--- Extras/CardMD3/app/src/main/java/com/example/cardmd3/MainActivity.java
+++ Extras/CardMD3/app/src/main/java/com/example/cardmd3/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.cardmd3;
 
 import android.os.Bundle;
+import android.widget.PopupMenu;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +15,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        ImageButton btnMore = findViewById(R.id.btnMore);
+        btnMore.setOnClickListener(v -> {
+            PopupMenu popup = new PopupMenu(this, v);
+            popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+            popup.setOnMenuItemClickListener(item -> {
+                if (item.getItemId() == R.id.op1) {
+                    // ação opção 1
+                    Toast.makeText(this, "Opção 1 selecionada", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (item.getItemId() == R.id.op2) {
+                    // ação opção 2
+                    Toast.makeText(this, "Opção 2 selecionada", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (item.getItemId() == R.id.op3) {
+                    // ação opção 3
+                    Toast.makeText(this, "Opção 3 selecionada", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            });
+            popup.show();
         });
     }
 }
